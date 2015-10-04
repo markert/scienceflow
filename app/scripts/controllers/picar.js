09aa27a628d6c0a8e9a5f4ca82299395d1f868c3
Florian Markert <florian@scienceflow.de>
2015-10-04 20:51:13 +0200

    new version

---
+++ app/scripts/controllers/picar.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sflowApp.controller:WegblCtrl
+ * @description
+ * # WegblCtrl
+ * Controller of the sflowApp
+ */
+angular.module('sflowApp')
+  .controller('PicarCtrl', function ($scope) {});
